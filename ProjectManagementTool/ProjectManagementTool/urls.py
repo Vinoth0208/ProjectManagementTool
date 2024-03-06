50abae52dce23cf4ea70307d867d44e30d5534fe
Vinoth <vinothchennai97@gmail.com>
2024-03-06 22:18:52 +0530

    completed

--- ProjectManagementTool/ProjectManagementTool/urls.py
+++ ProjectManagementTool/ProjectManagementTool/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,4 +25,7 @@ urlpatterns = [
     path('', include('account.urls')),
     path('projects/', include('project.urls')),
     path('projects/<uuid:project_id>/',include('todolist.urls')),
-]
+    path('projects/<uuid:project_id>/<uuid:todolist_id>/',include('task.urls')),
+
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- ProjectManagementTool/project/urls.py
+++ ProjectManagementTool/project/urls.py
@@ -7,5 +7,11 @@ urlpatterns=[
     path('<uuid:pk>', views.project, name='project'),
     path('<uuid:pk>/edit/', views.edit, name='edit'),
     path('<uuid:pk>/delete/', views.delete, name='delete'),
+    path('<uuid:project_id>/upload_file/', views.upload_file, name='upload_file'),
+    path('<uuid:project_id>/files/<uuid:pk>/delete_file/', views.delete_file, name='delete_file'),
+    path('<uuid:project_id>/add_note/', views.add_note, name='add_note'),
+    path('<uuid:project_id>/notes/<uuid:pk>/', views.note_detail, name='note_detail'),
+    path('<uuid:project_id>/notes/<uuid:pk>/edit/', views.note_edit, name='note_edit'),
+    path('<uuid:project_id>/notes/<uuid:pk>/delete/', views.note_delete, name='note_delete'),
 
 ]
